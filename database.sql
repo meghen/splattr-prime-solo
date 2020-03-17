0476d8a2aa7e6794c0722f9dbda1ab6d50240050
Megan Henry <megangabriellehenry@gmail.com>
2020-03-17 09:48:03 -0500

    initial set up

---
+++ database.sql
@@ -0,0 +1,12 @@
+
+-- USER is a reserved keyword with Postgres
+-- You must use double quotes in every query that user is in:
+-- ex. SELECT * FROM "user";
+-- Otherwise you will have errors!
+
+-- Create a database called splattr_prime_solo
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL
+);
