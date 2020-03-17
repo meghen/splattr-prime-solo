3eebbe01a8021ff53dc3889a9d0815e3b41b5ac1
Megan Henry <megangabriellehenry@gmail.com>
2020-03-17 18:07:57 -0500

    cleaned up pages a little, started form

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -3,8 +3,25 @@ import React, {Component} from 'react';
 class AddMovie extends Component {
     render() {
         return (
-            <div className=''>
-                <h1>there will be a form to add movies here</h1>
+            <div className='add-movie-form'>
+                <form>
+                    <label>
+                        Title: 
+                        <br/><input/>
+                    </label><br/>
+                    <label>
+                        Director: 
+                        <br/><input/>
+                    </label><br/>
+                    <label>
+                        Release Year: 
+                        <br/><input/>
+                    </label><br/>
+                    <label>
+                        Review/Notes:
+                        <br/><textarea></textarea>
+                    </label>
+                </form>
             </div>
         )
     }
