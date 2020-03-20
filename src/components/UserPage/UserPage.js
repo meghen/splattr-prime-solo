7385f9e27b606efd6a93edc1965d0c5c2bd90c4e
Megan Henry <megangabriellehenry@gmail.com>
2020-03-20 13:41:08 -0500

    added modal on btn click

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -5,6 +5,11 @@ class UserPage extends Component {
   state = {
     movie: []
   }
+  closeModal=()=>{
+    let modal = document.getElementById("myModal");
+    let span = document.getElementsByClassName("close")[0];
+    modal.style.display = "none";
+  }
   componentDidMount(){
     this.getMovies();
   }
@@ -15,15 +20,34 @@ class UserPage extends Component {
     //routes to movies details page, passing movie's id in as url router
     this.props.history.push(`/details/${movie.id}`)
   }
+  selectList=()=>{
+    let modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  }
     render() {
         return (
             <div className='welcome'>
               <h2>Welcome, {this.props.reduxState.user.username}</h2>
               {this.props.reduxState.homepage.map(movie => 
-                  <div className="moviesList" key={movie.id} onClick={()=>this.getInfo(movie)}>
+                  <div className="moviesList" key={movie.id}>
                     <div className="moviesListInner"><img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt="Movie Poster"/></div>
-                    <div className="moviesListInner"><b>{movie.title}</b> <br></br>{movie.overview}<br></br></div>
+                    <div className="moviesListInner">
+                      <b>{movie.title}</b> 
+                      <br></br>
+                      {movie.overview}
+                      <br></br>
+                      <button onClick={this.selectList}>Add To List</button>
+                      <button onClick={()=>this.getInfo(movie)}>More</button>
+                      </div>
                   </div>)}
+
+                  {/* testing modal below */}
+                  <div id="myModal" className="modal">
+                    <div className="modal-content">
+                      <span onClick={this.closeModal} className="close">&times;</span>
+                      <p>Some text in the Modal..</p>
+                    </div>
+                  </div>
               {/* <p>Your ID is: {props.user.id}</p> */}
             </div>
         )
