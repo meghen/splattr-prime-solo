3838259dd20695f7c7c2294b14bf52e642733a44
Megan Henry <megangabriellehenry@gmail.com>
2020-03-24 13:40:30 -0500

    set up front end new list modal

--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -7,8 +7,16 @@ class Lists extends Component {
         titleEdit: false,
         title: ''
     }
+    closeModal=()=>{
+        let modal = document.getElementById("myModal");
+        modal.style.display = "none";
+    }
     componentDidMount(){
         this.getLists();
+    }
+    createList=()=>{
+        let modal = document.getElementById("myModal");
+        modal.style.display = "block";
     }
     deleteList=(title)=>{        
         this.props.dispatch({type: 'DELETE_LIST', payload: title.id})
@@ -23,6 +31,11 @@ class Lists extends Component {
     handleChange=(event)=>{
         this.setState({title: event.target.value})
     }
+    saveNewList=()=>{
+        let modal = document.getElementById("myModal");
+        modal.style.display = "none";
+        this.props.dispatch({type: 'CREATE_NEW_LIST', payload: this.state.title})
+    }
     toggleTitle=()=>{
         this.setState({titleEdit: !this.state.titleEdit})
     }
@@ -30,7 +43,7 @@ class Lists extends Component {
         return (
             <div className='lists'>
                 <h2>List page</h2>
-                <h3>Placeholder for ADD LIST</h3>
+                <button onClick={this.createList}>New List</button>
                 <div>
                     {this.props.reduxState.collections.map(listTitle => 
                         <div className="listDisplay">
@@ -50,6 +63,14 @@ class Lists extends Component {
                                     <button onClick={()=>this.deleteList(listTitle)}>Delete List</button>
                                 </span>
                             </>} 
+                            <div id="myModal" className="modal">
+                    <div className="modal-content">
+                      <span onClick={this.closeModal} className="close">&times;</span>
+                      <label> Title:
+                          <input onChange={this.handleChange}></input></label>                      
+                      <button onClick={this.saveNewList}>Save</button>
+                    </div>
+                  </div>
                         </div>)}
                 </div>
             </div>
