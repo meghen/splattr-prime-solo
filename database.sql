fc72aad14b99129bca6b94379bf860bc8ba022a6
Megan Henry <megangabriellehenry@gmail.com>
2020-03-26 10:47:24 -0500

    update README.md

--- database.sql
+++ database.sql
@@ -4,6 +4,13 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 
+
+
+--Create a new database called `splattr_prime_solo` and create the following tables:
+--If you would like to name your database something else, 
+--you will need to change `splattr_prime_solo` 
+--to the name of your new database name in `server/modules/pool.js`
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
